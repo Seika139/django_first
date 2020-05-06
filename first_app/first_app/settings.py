88f89cc0106904a15955a0d5c4ff98179c6fd6b1
Ubuntu <ubuntu@ip-172-31-47-18.ap-northeast-1.compute.internal>
2020-05-06 01:08:49 +0000

    :bento: Revise settings.py

--- first_app/first_app/settings.py
+++ first_app/first_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'nuc@(0d#jo&9-u7mo0kwwb2g_&_r=^l8z#tvco1h(7a6#iqzio'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.114.88.188']
 
 
 # Application definition
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'first_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'firstapp',
+	'USER': 'firstappuser',
+	'PASSWORD': 'qwert',
+	'host': 'localhost',
+	'post': '', 
     }
 }
 
